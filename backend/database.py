f896352ada9db4cd406cf91d45babd8791bf62ee
Libisha Shirani <libisha01@gmail.com>
2026-04-21 18:32:33 +0530

    Added database connectivity, get_course and add_course endpoints

---
+++ backend/database.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+        host = os.getenv("DB_HOST")
+        port = os.getenv("DB_PORT")
+        user = os.getenv("DB_USER")
+        password = os.getenv("DB_PASSWORD")
+        database = os.getenv("DB_NAME")
+        autocommit = True
+    )
+
+def query(sql, params=(), fetchall = True):
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(sql, params)
+    result = cursor.fetchall() if fetchall else cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return result
+
+def execute(sql, params=()):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(sql, params)
+    last_id = cursor.lastrowid
+    cursor.close()
+    conn.close()
+    return last_id
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import database as db
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(title="ProgressTube API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["https://localhost:5173"],
+    allow_headers = ["*"],
+    allow_methods = ["*"]
+)
+
+class AddCourseRequest(BaseModel):
+    youtube_url: str
+    thumbnail: str
+    title: str
+    total_duration_seconds: int = 0
+    total_videos: int = 1
+    type: str = 'video'
+
+class UpdateProgressRequest(BaseModel):
+    watched_seconds: int
 
 @app.get("/")
 def root():
     return {"message" : "Progress Tube backend is live!"}
+
+@app.get("/courses")
+def get_courses():
+    courses = db.query("SELECT * FROM courses ORDER BY created_at DESC")
+    for c in courses:
+        total = c["total_duration_seconds"]
+        watched = c["watched_seconds"]
+        c["completed_percentage"] = round((watched / total * 100) if total > 0 else 0, 1)
+    return courses
+
+@app.post("/courses")
+def add_courses(req: AddCourseRequest):
+    course_id = db.query(
+        "INSERT INTO courses(youtube_url, thumbnail, title, total_duration_seconds, total_videos, type) VALUES (%s, %s, %s, %s, %s, %s)",
+        (req.youtube_url, req.thumbnail, req.title, req.total_duration_seconds, req.total_videos, req.type)
+    )
+    return {"message" : "Course added", "course_id" : course_id}
+
