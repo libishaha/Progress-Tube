945fbc6066e9c1545bf0b1e65799e37dc62da3fa
Libisha Shirani <libisha01@gmail.com>
2026-04-24 22:40:56 +0530

    Added parse seconds and get video metadata(duration, thumbnail) functions

--- backend/youtube_service.py
+++ backend/youtube_service.py
@@ -22,3 +22,26 @@ def extract_video_id(url):
 def extract_playlist_id(url):
     match = re.search(r"list=([A-Za-z0-9_-]+)", url)
     return match.group(1) if match else None
+
+def parse_duration(iso_duration):
+    pattern = r"PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?"
+    match = re.match(pattern, iso_duration)
+
+    if not match:
+        return 0
+    hours = int(match.group(1) or 0)
+    minutes = int(match.group(2) or 0)
+    seconds = int(match.group(3) or 0)
+
+    return hours * 3600 + minutes * 60 + seconds
+
+def get_video_metadata(video_id):
+    resp = requests.get(f"{BASE_URL}/videos", params = {
+        "key" : API_KEY,
+        "id" : video_id,
+        "part" : "snippet,contentDetails",
+    })
+
+    print(resp.json())
+
+get_video_metadata("VXtjG_GzO7Q")
